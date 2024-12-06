c779e626791c093ae08345d9a6254b6fae4cc158
Adalex3 <adalex3@icloud.com>
2024-12-05 23:20:42 -0500

    Logout testing

--- frontend/src/components/ProfileDisplay.tsx
+++ frontend/src/components/ProfileDisplay.tsx
@@ -5,13 +5,19 @@ interface ProfileDisplayProps {
   username: string;
 }
 
+function doLogout(event: any): void {
+  event.preventDefault();
+  location.href = '/';
+  localStorage.setItem("user_id","");
+};
+
 const ProfileDisplay: React.FC<ProfileDisplayProps> = ({ username }) => {
   return (
     <div className="profile-display">
       <img src="../src/assets/person.png"></img>
       <div className="">
         <p>{username}</p>
-        <button>Sign out</button>
+        <button onClick={doLogout}>Sign out</button>
       </div>
     </div>
   );
